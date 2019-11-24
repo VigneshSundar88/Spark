c7c7367eaf98f4b71c07c3037054c6f5590ad3f2
VigneshSundar88 <mailtovignesh88@gmail.com>
2019-11-25 01:34:42 +0530

    Update prog_3(fixed).py

--- prog_3(fixed).py
+++ prog_3(fixed).py
@@ -2,8 +2,8 @@ from pyspark.sql.functions import substring
 
 class parse_file():
     def __init__(self):
-        self.fixed_length_schema = "https://github.com/VigneshSundar88/Spark/blob/master/Dataset/fixed_data.csv"
-        self.fixed_length_file = "https://github.com/VigneshSundar88/Spark/blob/master/Dataset/fixed_length_file.txt"
+        self.fixed_length_schema = "/FileStore/tables/fixed_data.csv"
+        self.fixed_length_file = "/FileStore/tables/fixed_length_file.txt"
       
     def create_df(self):
         read_fixed_schema = spark.read.format('csv').\
@@ -11,11 +11,13 @@ class parse_file():
             option("inferSchema", "true").\
             load(self.fixed_length_schema)
  
+
         read_fixed_length_file = spark.read.format("csv").\
             option("header", "false").\
             load(self.fixed_length_file).\
             toDF("fixed_line")
-           
+         
+        
         self.substr_file(read_fixed_schema, read_fixed_length_file) 
   
     def substr_file(self, read_fixed_schema, read_fixed_length_file):
@@ -29,8 +31,10 @@ class parse_file():
                     len = int(header['length'])).\
                     alias(header['Col_Name'])
                     for header in schema_as_dict
-                    ]).show()
-
+                    ])
+        create_sub_df.write.mode('overwrite').saveAsTable("parsed_tbl")
+        df_select = spark.sql("select * from parsed_tbl").show()
+      
 if __name__ == "__main__":
     parse_rec = parse_file()
     parse_rec.create_df()
